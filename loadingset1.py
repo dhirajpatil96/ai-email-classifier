0cb1497cd62f94fa7f12eb8c7aaac067e0707618
Dhiraj Kajale <dhirajkajale4@gmail.com>
2025-12-18 18:30:18 +0530

    Update loadingset1.py

--- loadingset1.py
+++ loadingset1.py
@@ -1,10 +1,10 @@
 import pandas as pd
-df = pd.read_csv('D:\email dataset\enron\emails.csv')  # Adjust path
-# Assume 'message' or similar column has raw email text
+df = pd.read_csv('D:\email dataset\enron\emails.csv')  #path of the dataset
 if 'message' in df.columns:
          df = df.rename(columns={'message': 'raw_email'})
-df = df[['raw_email']]  # Keep only needed column
+df = df[['raw_email']]  
 df.to_csv('raw_emails.csv', index=False)
 print(f"Loaded {len(df)} emails from CSV.")
 print('Emails saved to raw_emails.csv')
+
      
