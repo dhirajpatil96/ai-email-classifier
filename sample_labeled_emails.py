e96240db636fc3b6f2524b075897c9d608d33460
Dhiraj Kajale <dhirajkajale4@gmail.com>
2026-01-12 13:43:55 +0530

    Delete sample_labeled_emails.py

--- sample_labeled_emails.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("labeled_emails.csv")
-
-sample_df = df.sample(n=5000, random_state=42)
-
-sample_df.to_csv("labeled_emails_sample.csv", index=False)
-
-print("Sample file created")
